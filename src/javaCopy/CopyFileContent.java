33681bf6f19aa1c216258947bbb307bbaffda1c0
ritesh konduru <riteshkonduru@gmail.com>
2017-11-15 15:29:58 +0530

    j

--- src/javaCopy/CopyFileContent.java
+++ src/javaCopy/CopyFileContent.java
@@ -7,7 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-//hello.
+//hello
 public class CopyFileContent {
 
 	public static void main(String[] args) {
